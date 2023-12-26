b970b8f765500ec4ea417bfb1107b8d747cb9f65
sabbir1054 <mdsabbir1054@gmail.com>
2023-12-26 17:00:36 +0600

    auth and jwt setup

--- src/config/index.ts
+++ src/config/index.ts
@@ -7,4 +7,12 @@ export default {
   port: process.env.PORT,
   databaseUrl: process.env.DATABASE_URL,
   default_user_pass: process.env.USER_DEFAULT_PASS,
+  bycrypt_salt_rounds: process.env.BCRYPT_SALT_ROUNDS,
+  base_url_frontend: process.env.BASE_URL_FRONTEND,
+  jwt: {
+    secret: process.env.JWT_SECRET,
+    refresh_secret: process.env.JWT_REFRESH_SECRET,
+    expires_in: process.env.JWT_EXPIRES_IN,
+    refresh_expires_in: process.env.JWT_REFRESH_EXPIRES_IN,
+  },
 };

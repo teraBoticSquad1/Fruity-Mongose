df16258a492eaa387200389f7947c3c08f45a194
sabbir1054 <mdsabbir1054@gmail.com>
2023-12-28 18:10:14 +0600

    impliment products model

--- src/app/modules/products/product.inteface.ts
+++ src/app/modules/products/product.inteface.ts
@@ -1,5 +1,6 @@
-import { Types } from 'mongoose';
+import { Model, Types } from 'mongoose';
 import { ICategory } from '../category/category.interface';
+import { IReview } from '../review/review.interface';
 
 export type IProduct = {
   code: string;
@@ -10,4 +11,7 @@ export type IProduct = {
   photo: string;
   gallery: string[];
   category: Types.ObjectId[] | ICategory[];
+  reviews: Types.ObjectId[] | IReview[];
 };
+
+export type ProductModel = Model<IProduct, Record<string, unknown>>;
---
+++ src/app/modules/products/product.model.ts
@@ -0,0 +1,59 @@
+import { Schema, Types, model } from 'mongoose';
+import { IProduct, ProductModel } from './product.inteface';
+import { productSchema } from './product.model';
+
+export const productSchema = new Schema<IProduct>(
+  {
+    code: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    stock: {
+      type: Number,
+      required: true,
+    },
+    photo: {
+      type: String,
+      required: true,
+    },
+    gallery: [
+      {
+        type: String,
+        required: true,
+      },
+    ],
+    category: [
+      {
+        type: Types.ObjectId,
+        ref: 'Category', // replace 'Category' with the actual model name for categories
+      },
+    ],
+    reviews: [
+      {
+        type: Types.ObjectId,
+        ref: 'Review',
+      },
+    ],
+  },
+  {
+    timestamps: true,
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+
+export const Product = model<IProduct, ProductModel>('Product', productSchema);
